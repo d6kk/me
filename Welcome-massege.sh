099d72b3f8ab3da01e1ed7d4e4cbcef8e6c06d6f
Dk <82993532+d6kk@users.noreply.github.com>
2026-08-31 00:06:24 +0530

    Create Welcome-massege.sh

---
+++ Welcome-massege.sh
@@ -0,0 +1,21 @@
+## zsh ho to direct chalega ## 
+cat <<'E'>> /data/data/com.termux/files/usr/etc/zshrc
+## defult welcome massage remove ##
+if [ -f "/data/data/com.termux/files/usr/etc/motd" ]; then
+    rm -rf /data/data/com.termux/files/usr/etc/motd
+fi
+## important pkg download ##
+if ! command -v neofetch > /dev/null 2>&1; then
+    pkg install neofetch -y > /dev/null 2>&1
+fi
+## important pkg download ##
+if ! command -v figlet > /dev/null 2>&1; then
+    pkg install figlet -y > /dev/null 2>&1
+fi
+## Show welcome massage command ##
+clear
+neofetch
+figlet -f standard   WELCOME
+figlet -f standard DARKILLER
+E
+source $PREFIX/etc/zshrc
