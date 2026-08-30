847f045d53970b5d50eff0fc00658d1f943a5ff8
Dk <82993532+d6kk@users.noreply.github.com>
2026-08-31 03:01:16 +0530

    Update style.sh

--- style.sh
+++ style.sh
@@ -15,6 +15,7 @@ cp "$HOME/.oh-my-zsh/templates/zshrc.zsh-template" "$HOME/.zshrc"
 sed -i '/^ZSH_THEME/d' "$HOME/.zshrc"
 sed -i '1iZSH_THEME="agnoster"' "$HOME/.zshrc"
 
+# Install syntax highlighting plugin
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME/.zsh-syntax-highlighting" --depth 1
 echo "source $HOME/.zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> "$HOME/.zshrc"
 
