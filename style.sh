4cff2979681ac11cb489deeb981f28bb02d68b91
Dk <82993532+d6kk@users.noreply.github.com>
2026-09-01 01:25:30 +0530

    Update style.sh

--- style.sh
+++ style.sh
@@ -25,25 +25,4 @@ echo zsh | chsh -s zsh 2>/dev/null || true
 # Apply color and font settings
 printf '0\n' | bash "$HOME/.termux/colors.sh" 2>/dev/null
 printf '14\n' | bash "$HOME/.termux/fonts.sh" 2>/dev/null
-## zsh ho to direct chalega ## 
-cat <<'E'>> /data/data/com.termux/files/usr/etc/zshrc
-## defult welcome massage remove ##
-if [ -f "/data/data/com.termux/files/usr/etc/motd" ]; then
-    rm -rf /data/data/com.termux/files/usr/etc/motd
-fi
-## important pkg download ##
-if ! command -v neofetch > /dev/null 2>&1; then
-    pkg install neofetch -y > /dev/null 2>&1
-fi
-## important pkg download ##
-if ! command -v figlet > /dev/null 2>&1; then
-    pkg install figlet -y > /dev/null 2>&1
-fi
-## Show welcome massage command ##
-clear
-neofetch
-figlet -f standard   WELCOME
-figlet -f standard DARKILLER
-E
-source $PREFIX/etc/zshrc
 exec zsh -i -l
