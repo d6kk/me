1ff20df1efd1ca402d50d78b52546eddaf132eb7
Dk <82993532+d6kk@users.noreply.github.com>
2026-09-01 01:33:03 +0530

    Update style.sh

--- style.sh
+++ style.sh
@@ -1,22 +1,20 @@
 #!/data/data/com.termux/files/usr/bin/bash
 # Setup storage and install packages
 termux-setup-storage
-apt update
-apt install -y git zsh
+apt install -y git zsh >/dev/null 
 
 # Install Termux Oh-My-Zsh theme
-git clone https://github.com/Cabbagec/termux-ohmyzsh.git "$HOME/termux-ohmyzsh" --depth 1
+git clone https://github.com/Cabbagec/termux-ohmyzsh.git "$HOME/termux-ohmyzsh" --depth 1 >/dev/null
 cp -R "$HOME/termux-ohmyzsh/.termux" "$HOME/.termux"
 
 # Setup Zsh configuration
-git clone https://github.com/ohmyzsh/ohmyzsh.git "$HOME/.oh-my-zsh" --depth 1
-mv "$HOME/.zshrc" "$HOME/.zshrc.bak.$(date +%Y.%m.%d-%H:%M:%S)"
+git clone https://github.com/ohmyzsh/ohmyzsh.git "$HOME/.oh-my-zsh" --depth 1 >/dev/null
 cp "$HOME/.oh-my-zsh/templates/zshrc.zsh-template" "$HOME/.zshrc"
 sed -i '/^ZSH_THEME/d' "$HOME/.zshrc"
 sed -i '1iZSH_THEME="agnoster"' "$HOME/.zshrc"
 
 # Install syntax highlighting plugin
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME/.zsh-syntax-highlighting" --depth 1
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME/.zsh-syntax-highlighting" --depth 1 >/dev/null
 echo "source $HOME/.zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> "$HOME/.zshrc"
 
 # Set default shell
